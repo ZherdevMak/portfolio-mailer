caccad7bb94a4027db20a164ec039c7b86a4657d
ZherdevMak <Zherdev.mak@gmail.com>
2022-09-26 21:38:11 +0300

    Mailer complited

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require ('express');
 const nodemailer = require("nodemailer");
 const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// async function main() {
 let transporter = nodemailer.createTransport({
     // host: "smtp.yandex.ru",
     // port: "465",
@@ -12,23 +18,39 @@ let transporter = nodemailer.createTransport({
         pass: 'mjjohccpbtytwbxv', // generated ethereal password
     },
     tls:{rejectUnauthorized:false}
+
 })
 
 
 
-app.get ('/sendmail', async function (req,res) {
-    let info = await transporter.sendMail({
+app.post ('/sendmail', function (req,res) {
+    let {name, email, subject, message} = req.body
+    let info = transporter.sendMail({
         from: 'me', // sender address
         to: "ppmaxjer@yandex.ru", // list of receivers
         subject: "Hello ✔", // Subject line
         text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-    });
+        html: `<b>Сообщение от вашего Portfolio</b>
+         <div>${name}</div>
+         <div>${email}</div>
+         <div>${subject}</div>
+         <div>${message}</div>`
+    })
+    .then(res => {
+        console.log(info)
+    })
 })
 
-app.listen(3000,function () {
-    console.log('Listen for port 3000!node')
+app.listen(3001,function () {
+    console.log('Listen for port 3001!node')
+    // console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 })
+// }
+// main().catch(console.error);
 
 
 

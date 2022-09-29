84f4b9ed2efaaeb0c7cb289d2043c3aa586b54ef
ZherdevMak <Zherdev.mak@gmail.com>
2022-09-29 13:54:38 +0300

    heroku deploy smtp login/password

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const bodyParser = require('body-parser')
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-// async function main() {
+let smtp_login = process.env.SMTP_LOGIN
+let smtp_password = process.env.SMTP_PASSWORD
 let transporter = nodemailer.createTransport({
     // host: "smtp.yandex.ru",
     // port: "465",
     // secure: true,
     service: 'gmail', auth: {
-        user: 'zherdev.max@gmail.com', // generated ethereal user
-        pass: 'mjjohccpbtytwbxv', // generated ethereal password
+        user: smtp_login, // generated ethereal user
+        pass: smtp_password, // generated ethereal password
     }, tls: {rejectUnauthorized: false}
 
 })
@@ -100,17 +101,10 @@ app.post('/sendmail', function (req, res) {
         return res.end();
     })
 })
-let port = process.env.PORT
+let port = process.env.PORT || 3001
 app.listen(port, function () {
-    console.log('Listen for port 3001!node')
-    // console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log('Listen for port 3001!node')})
 
-    // Preview only available when sending through an Ethereal account
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-})
-// }
-// main().catch(console.error);
 
 
 

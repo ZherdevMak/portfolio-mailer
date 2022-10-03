74bff9fd85db0d8ea2143883c636378d08d37c08
ZherdevMak <Zherdev.mak@gmail.com>
2022-10-03 20:20:15 +0300

    test commit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/sendmail', function (req, res) {
     transporter.sendMail({
         from: 'me', // sender address
         to: "zherdev.max@gmail.com", // list of receivers
-        subject: "Portfolio message", // Subject line
+        subject: "Portfolio message!", // Subject line
         text: "Portfolio mail", // plain text body
         html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
